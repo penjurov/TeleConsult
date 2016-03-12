73e0ecb29ec0886f1a7e7f6dc7a50c35b361dd99
Penjurov <iliapenjurov@gmail.com>
2016-03-12 10:08:10 +0200

    Add skiped by prior Commit Models

---
+++ Web/TeleConsult.Web/Areas/Admin/Models/Base/AdminModel.cs
@@ -0,0 +1,38 @@
+﻿namespace TeleConsult.Web.Areas.Admin.Models.Base
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using Common;
+    using Web.Models;
+    using Web.Models.Base;
+    
+    public class AdminModel : BaseModel, IModel<bool>
+    {
+        public IEnumerable<SelectListItem> Statuses { get; set; }
+
+        public virtual void Init(bool init)
+        {
+            if (init)
+            {
+                if (init)
+                {
+                    this.Statuses = new List<SelectListItem>()
+                {
+                    new SelectListItem
+                    {
+                        Text = GlobalConstants.Statuses.Active,
+                        Value = bool.FalseString,
+                        Selected = true
+                    },
+                    new SelectListItem
+                    {
+                        Text = GlobalConstants.Statuses.Deleted,
+                        Value = bool.TrueString
+                    },
+                };
+                }
+            }
+        }
+    }
+}
---
+++ Web/TeleConsult.Web/Areas/Admin/Models/SpecialtyModel.cs
@@ -0,0 +1,103 @@
+﻿namespace TeleConsult.Web.Areas.Admin.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Validation;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Base;
+    using Common;
+    using Data.Filters.Admin;
+    using Data.Models;
+    using Data.Proxies;
+    using Data.Repositories;
+    using Web.Models;
+
+    public class SpecialityModel : AdminModel, IModel<bool>
+    {
+        public SpecialityProxy ViewModel { get; set; }
+
+        public List<SpecialityProxy> GetSpecialities(AdminFilter filter)
+        {
+            return this.RepoFactory.Get<SpecialityRepository>().Get(filter).ToList();
+        }
+
+        public int Save(SpecialityProxy proxy, ModelStateDictionary modelState)
+        {
+            if (proxy != null && modelState.IsValid)
+            {
+                try
+                {
+                    var repo = this.RepoFactory.Get<SpecialityRepository>();
+                    Speciality speciality;
+
+                    var existing = repo.SpecialityExist(proxy.Name, proxy.Id);
+
+                    if (existing)
+                    {
+                        throw new Exception(GlobalConstants.Errors.SpecialityExist);
+                    }
+
+                    if (proxy.Id.HasValue)
+                    {
+                        speciality = repo.GetById(proxy.Id);
+                    }
+                    else
+                    {
+                        speciality = new Speciality();
+                        repo.Add(speciality);
+                    }
+
+                    speciality.Name = proxy.Name;
+
+                    repo.SaveChanges();
+
+                    return speciality.Id;
+                }
+                catch (DbEntityValidationException e)
+                {
+                    throw new Exception(this.HandleDbEntityValidationException(e));
+                }
+            }
+            else
+            {
+                throw new Exception(this.HandleErrors(modelState));
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var repo = this.RepoFactory.Get<SpecialityRepository>();
+            var speciality = repo.GetById(id);
+
+            if (speciality != null)
+            {
+                speciality.IsDeleted = true;
+                speciality.DeletedOn = DateTime.Now;
+                repo.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Специалност с такова Id не съществува");
+            }
+        }
+
+        public void Activate(int id)
+        {
+            var repo = this.RepoFactory.Get<SpecialityRepository>();
+            var speciality = repo.GetById(id);
+
+            if (speciality != null)
+            {
+                speciality.IsDeleted = false;
+                speciality.DeletedOn = null;
+                repo.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Специалност с такова Id не съществува");
+            }
+        }
+    }
+}
